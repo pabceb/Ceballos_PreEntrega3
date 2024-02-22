37d5e729d31dd5885cdb6f253cd8774e4204c131
PabCeb <pablo.ceballos.08@gmail.com>
2024-02-22 11:14:03 -0300

    modificaciones en formularios y web

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from inicio.views import inicio, productos, clientes
+from inicio.views import inicio, productos, clientes, crear_cliente, crear_producto, mostrar_clientes, mostrar_productos
 
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('productos/', productos, name='productos'),
     path('clientes/', clientes, name='clientes'),
+    path('productos/crear-producto', crear_producto, name='crear_producto'),
+    path('clientes/crear-cliente', crear_cliente, name='crear_cliente'),
+    path('clientes/mostrar-productos', mostrar_productos, name='mostrar_productos'),
+    path('clientes/mostrar-clientes', mostrar_clientes, name='mostrar_clientes'),
+    
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-# from django.http import HttpResponse
+from django.http import HttpResponse
 # from django.template import Template, Context, loader
 
 from inicio.models import Cliente, Producto
@@ -10,10 +10,13 @@ def inicio(request):
     return render(request, 'inicio.html', {})
 
 def productos(request):
-    return render(request, 'productos.html', {})
+    productos = Producto.objects.all()
+    return render(request, 'productos.html', {'productos': productos})
+
 
 def clientes(request):
-    return render(request, 'clientes.html', {})
+    clientes = Cliente.objects.all()
+    return render(request, 'clientes.html', {'clientes': clientes})
 
 def crear_producto(request):
     formulario = FormularioCrearProducto()
@@ -26,8 +29,7 @@ def crear_producto(request):
         producto = Producto(prod_id = prod_id, nombre = nombre, marca = marca, precio = precio)
         producto.save()
         return redirect('productos.html')
-    return render(request, 'productos.html', {'formulario': formulario})
-    # return render(request, 'crear_producto. html', {'formulario': formulario})
+    return render(request, 'crear_producto.html', {'formulario': formulario})
 
 def crear_cliente(request):
     formulario = FormularioCrearCliente()
@@ -39,13 +41,14 @@ def crear_cliente(request):
         cliente = Cliente(nombre = nombre, apellido = apellido, telefono = telefono)
         cliente.save()
         return redirect('clientes.html')
-    return render(request, 'clientes.html', {'formulario': formulario})
-    # return render(request, 'crear_clientes. html', {'formulario': formulario})
+    return render(request, 'crear_cliente.html', {'formulario': formulario})
 
-def mostrar_producto(request):
-    productos = Producto.objects.all()
-    return render(request, 'mostrar_producto.html', {'productos': productos})
+def mostrar_productos(request):
+    return HttpResponse('Ceballos - PreEntrega3 - Productos')
+#     productos = Producto.objects.all()
+#     return render(request, 'productos.html', {'productos': productos})
 
 def mostrar_clientes(request):
-    clientes = Cliente.objects.all()
-    return render(request, 'mostrar_clientes.html', {'clientes': clientes})
+    return HttpResponse('Ceballos - PreEntrega3 - Clientes')
+#     clientes = Cliente.objects.all()
+#     return render(request, 'clientes.html', {'clientes': clientes})
